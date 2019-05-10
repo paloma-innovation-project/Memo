d7259a394734faa126f9ebde60b49eee7898c9df
Shingo Morimoto <shingo.morimoto@paloma.co.jp>
2019-05-10 15:11:27 +0900

    初回リリース

--- Memo/Form1.cs
+++ Memo/Form1.cs
@@ -16,5 +16,33 @@ namespace Memo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private string FileRead(string path)
+        {
+            //string path = @"C:\Users\MORIMOTOS\Desktop\新しいフォルダー (2)\test.txt";
+            var enc = System.Text.Encoding.GetEncoding("shift-jis");
+            System.IO.StreamReader sr = new System.IO.StreamReader(path, enc);
+            //System.IO.StreamReader sr = new System.IO.StreamReader(path);
+            string txt = sr.ReadToEnd();
+            return txt;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void 開くOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = FileRead(openFileDialog1.FileName);
+            }
+        }
     }
 }
